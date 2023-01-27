acc2cae97eefd0ef9e2856ca106e6004d5f489a5
MihaelaIancu <iancumihaela46@yahoo.com>
2023-01-27 06:02:10 +0200

    Sidebar integration

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -25,7 +25,7 @@ import {
   Icon,
 } from "@mui/material";
 
-const Navbar = () => {
+const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
   const dispatch = useDispatch();
   const theme = useTheme();
 
@@ -40,7 +40,7 @@ const Navbar = () => {
       <Toolbar sx={{ justifyContent: "space-between" }}>
         {/* {LEFT SIDE} */}
         <FlexBetween>
-          <IconButton onCLick={() => console.log("open/close sidebar")}>
+          <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
             <MenuIcon />
           </IconButton>
           <FlexBetween
